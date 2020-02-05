45baca898ba73d6e04d3086d5e988b8fc9721dfa
Riedl, Christoph <criedl@edu.aau.at>
2020-02-05 15:27:36 +0100

    Added stoppwatch function for cardio

--- client/src/app/trainingsansicht/trainingsansicht.component.ts
+++ client/src/app/trainingsansicht/trainingsansicht.component.ts
@@ -18,7 +18,8 @@ export class ConfigService {
 
 
 export class TrainingsansichtComponent implements OnInit {
-  private timeLeft: number = 3;
+  private timeLeft;
+  private timeString;
   private interval;
   private username = localStorage.getItem("u_name");
 
@@ -48,65 +49,54 @@ export class TrainingsansichtComponent implements OnInit {
     })
     this.initializeSets();
    
-  }
-
-
-  
-  startWorkout(): void{
+  }  
+  startTimer(): void{
     this.buttonDisabled = true; 
-
+    this.timeString = this.timeLeft = 3;
     this.interval = setInterval(() => {
       if(this.timeLeft > 0) {
-        this.timeLeft--;
+        this.timeString=this.timeLeft--;
       } else {
-        this.timeLeft = 3;
+        this.timeString = this.timeLeft = 3;
         clearInterval(this.interval);
         this.buttonDisabled = false;      
       }
     },1000)
 
   }
-
   initializeSets() {
-    this.workout.getSets(this.workout_id).subscribe((data) => {
+    this.workout.getSets(this.workout_id).subscribe((workout) => {
 
      this.sets= [];
      this.exerciseCounter = 0;
-
-      for (let exercises of data) {
-
+      for (let exercises of workout) {
         let element = exercises.sets;
-        console.log(exercises);
-        console.log(exercises.exercise_name);
-        console.log(exercises.iscardio)
-
         for (let i=1; i<=element;i++)
         this.sets.push({"exercise_name" : exercises.exercise_name,
                         "setnumber" : i,
                         "iscardio" : exercises.iscardio
         })
-
-
       }
       console.log(this.sets);
-
       this.currentExercise = this.sets[0];
       this.iscardio = this.currentExercise.iscardio;
-      console.log(this.currentExercise.exercise_name);
-      console.log(this.currentExercise.setnumber);
-      console.log(this.currentExercise.iscardio)
-
+      if(!this.iscardio){
+        this.timeString = this.timeLeft = 3
+      }else {
+        this.timeString = this.timeLeft = 0
+      }
     });
   }
-  nextSet(): void{
-       
+  nextSet(): void{   
+
     if(this.exerciseCounter <this.sets.length-1){
-    this.startWorkout(); 
+      if(!this.currentExercise.iscardio){
+        this.startTimer();
+      } 
     this.insertLogEntry();
     this.exerciseCounter = this.exerciseCounter+1;
     this.currentExercise = this.sets[this.exerciseCounter];
     this.iscardio = this.currentExercise.iscardio;
-
   } else{
     //hier noch route ändern
     this.insertLogEntry();
@@ -115,17 +105,15 @@ export class TrainingsansichtComponent implements OnInit {
   }
   }
 
-  insertLogEntry(){
-    let username = this.username;
-    let exercisename = this.currentExercise.exercise_name;
-    let iscardio = this.currentExercise.iscardio;
-    let setnumber = this.currentExercise.setnumber
-    let options = { day: '2-digit', year: 'numeric', month: '2-digit' };
-    let today  = new Date();
+  insertLogEntry(){ 
+    
+    // Datum wird in Node.js erstellt
+    //let options = { day: '2-digit', year: 'numeric', month: '2-digit' };
+    //let today  = new Date();
     //Chinesische Reihenfolge, weil sie zum Postgres Format passt
-    let formattedDate = today.toLocaleDateString("zh-Hans-CN", options).replace(/[/:.-]+/gi, '-')
-    console.log(formattedDate);
-     //reps and weight
+    //let formattedDate = today.toLocaleDateString("zh-Hans-CN", options).replace(/[/:.-]+/gi, '-')
+    //console.log(formattedDate);
+     //reps, weight, duration
     if(!this.iscardio){
       this.weight =(<HTMLInputElement>document.getElementById("weightInput")).value;
       this.reps = (<HTMLInputElement>document.getElementById("repInput")).value;
@@ -135,11 +123,42 @@ export class TrainingsansichtComponent implements OnInit {
     else{
       this.weight = null;
       this.reps = null;
-      this.duration = (<HTMLInputElement>document.getElementById("durationInput")).value;
+      this.duration = (this.timeLeft/60).toFixed(2)
       console.log("duration: " + this.duration)
-    }
-        
-    this.dataservice.insertLogEntry(username,exercisename,iscardio,setnumber,this.weight,this.reps,this.workout_id, this.duration).subscribe(data=>{
+    }        
+    this.dataservice.insertLogEntry(this.username,this.currentExercise.exercise_name,this.currentExercise.iscardio,this.currentExercise.setnumber,this.weight,this.reps,this.workout_id, this.duration).subscribe(data=>{
     });
   }
+
+
+  startStopwatch():void{
+    console.log("in stoppwatch")
+    let timerButton = (<HTMLInputElement>document.getElementById("timer_btn"))
+       
+    if (timerButton.innerHTML =="Stoppuhr starten"){
+      timerButton.innerHTML = "Stoppuhr stoppen"
+      //timer starten
+      this.timeString = this.timeLeft = 0;
+      this.interval = setInterval(() => {        
+          this.timeLeft = this.timeLeft+1
+          if(this.timeLeft>60){
+            let minutes = Math.floor(this.timeLeft/60)
+            let seconds = this.timeLeft%60
+            this.timeString = minutes + " : " + seconds
+          } else{
+            this.timeString = this.timeLeft
+          }         
+      },1000)
+    }else if (timerButton.innerHTML == "Stoppuhr stoppen"){
+      timerButton.innerHTML = "Cardio speichern"
+      clearInterval(this.interval);
+      //timer stoppen
+    }else {
+      console.log("im speichern")
+      timerButton.innerHTML ="Stoppuhr starten"
+     this.nextSet();
+      //nächster satz
+    }    
+  }
+
 }
